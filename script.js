b5519e53ef08c32ee7cc8e6c0079472cbed7b9d5
Flavien Roussel <flavien.roussel@etu.univ-poitiers.fr>
2023-09-19 17:44:39 +0200

    titre
    Améliore la gestion du titre

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("oui")
+
+changeTitle();
+/**
+ * Change the title of the page
+ */
+function changeTitle() {
+    const title = document.getElementById("title");
+    title.innerText += ' from JS';
+    title.style.color = 'red';
+}
