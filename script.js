7aa7be8ffad89f97684fb0d446ca3c648593ee8a
Flavien Roussel <flavien.roussel@etu.univ-poitiers.fr>
2023-09-19 18:09:34 +0200

    Ajout sous titre

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function changeTitle() {
     title.innerText += ' from JS';
     title.style.color = 'red';
 }
+
+// Add a h2 element under the h1
+const h2 = document.createElement("h2");
+h2.id = "sous-titre";
+h2.innerText = "Welcome to the javascript world";
+document.querySelector("h1").after(h2);
